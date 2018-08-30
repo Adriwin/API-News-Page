f006eadef59d7a09d753feac66963a2e4eb23b5a
Łukasz B <lukasbulawskidev@gmail.com>
2018-08-30 21:21:07 +0200

    arrow to API, res and show items, without styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import Articles from './Articles/Articles';
 
-export default class App extends Component {
+
+const config = {
+  url :'https://newsapi.org/v1/articles?source=the-verge&',
+  apiKey :"59b95200a2da4aa5991a90f431f28f2f"
+}
+class App extends Component {
+
+ constructor(props) {
+      super(props);
+        this.state = {
+            articles:[]
+        }
+  }
+  componentDidMount() {
+  	const currentUrl = config.url + 'apiKey=' + config.apiKey;	
+    fetch(currentUrl)
+      .then(res => res.json())
+      .then(json => this.setState({articles:json.articles}));
+  }
+
   render() {
+    console.log(this.state.articles)
     return (
-      <div>Let's get it done</div>
+      <div>
+      	<Articles articles={this.state.articles} />
+      </div>
     );
   }
 }
+
+
+export default App;
